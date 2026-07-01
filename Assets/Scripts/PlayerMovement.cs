9fdd5ca688ba97522d515d10023be7fb49bf3efd
ItzVihaan <vihaan.chattaraj@education.nsw.gov.au>
2026-07-01 10:35:12 +1000

    WIP(Fireball)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
 
     private void Awake()
     {
-        // Grab references for rigidbody, animator, and box collider from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
@@ -19,34 +22,69 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
-
+         horizontalInput = Input.GetAxis("Horizontal");
+      
         // Flip player left or right based on movement direction
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded())
-            Jump();
-
         // Sets animator parameters
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBppl("grounded", isGrounded());
+        anim.SetBool("grounded", isGrounded()); 
+        
+        // Cooldown timer should decrease every frame down to 0
+        if (wallJumpCooldown > 0)
+        {
+            wallJumpCooldown -= Time.deltaTime;
+        }
 
-        print(onWall());
+        // Only allow normal horizontal movement control if not currently pushing off a wall jump
+        if (wallJumpCooldown <= 0) 
+        {
+            body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 7;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+        }
     }
 
     private void Jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        anim.SetTrigger("jump");
+        if (isGrounded())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+
+         // Set the cooldown to temporarily stop player input during the push-off
+        {
+            if (horizontalInput == 0)
+            {
+              body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+              transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+                 body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            wallJumpCooldown = 0.2f; 
+            
+             
+            anim.SetTrigger("jump");
+        }
     }
 
-    private void OnCollisonEnter2D(Collision2D collision)
-    {
-    }
 
     private bool isGrounded()
     {
@@ -54,9 +92,14 @@ public class PlayerMovement : MonoBehaviour
         return raycastHit.collider != null;
     }
 
-    private bool onwall()
+    private bool onWall() 
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
     }
 }
